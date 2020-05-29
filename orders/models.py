7783d69135ff81b9136ff2c4a234e42d24f994f9
Oleksandr <oleksandrr@fornova.net>
2020-05-29 22:49:16 +0300

    Added an Account model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(unique=True, max_length=128, null=False, blank=False)
+    api_key = models.CharField(max_length=256, blank=False)
+    api_secret = models.CharField(max_length=256, blank=False)
+
+    def __str__(self):
+        return self.name
